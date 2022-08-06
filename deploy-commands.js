c3a6d9e82ed65c289ba09a8d267d3858c896ce33
Aeroshide <78432778+Aeroshide@users.noreply.github.com>
2022-08-06 13:21:33 +0700

    completely overhauled how bot works (WIP)
    * better logger
    * nested to 1 command (/findseed, still WIP)
    * global commands, so now works in every server ever
    * better backend stuff lol

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,24 +1,62 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-//const { clientId, guildId, token } = require('./config.json');]
 
+const isDebugEnv = require('./index.js')
+
+const token = process.env['token']
 const cid = process.env['clientID']
 const gid = process.env['guildID']
 
 
+reloadCommands();
 
-const commands = [
-	new SlashCommandBuilder().setName('mapless').setDescription('(1.16.1) Generate a Seed with close proximity to a Bur. Treasure (spawn island), With FSG Nether'),
-	new SlashCommandBuilder().setName('poweredvillage').setDescription('(1.19+) Generate a seed with a Guaranteed Village and Ruined Portal, Good nether with Warped Forest'),
-	new SlashCommandBuilder().setName('coastalvillage').setDescription('(1.15.2) seed with a Guaranteed Coastal, Monument And Stronghold in the + / + Direction'),
-	new SlashCommandBuilder().setName('botinfo').setDescription('Shows you bot infos! (some are important)'),
-]
-	.map(command => command.toJSON());
+function reloadCommands() {
+	const commands = [
+		/*
+		new SlashCommandBuilder().setName('mapless').setDescription('(1.16.1) Generate a Seed with close proximity to a Bur. Treasure (spawn island), With FSG Nether'),
+		new SlashCommandBuilder().setName('poweredvillage').setDescription('(1.19+) Generate a seed with a Guaranteed Village and Ruined Portal, Good nether with Warped Forest'),
+		new SlashCommandBuilder().setName('coastalvillage').setDescription('(1.15.2) seed with a Guaranteed Coastal, Monument And Stronghold in the + / + Direction'),
+		/**/
+		new SlashCommandBuilder()
+		.setName("findseed")
+		.setDescription("Generate a seed with a custon filter!")
+		.addStringOption((option) => option
+				.setName("filter")
+				.setDescription("Select what filter do you want to generate for")
+				.setRequired(true)
+				.addChoices(
+					{ name: "Mapless [1.16.1, Generate a seed with mapless and FSG Nether]", value: "mapless" },
+					{ name: "test", value: "yeah" },
+			)),
 
-const rest = new REST({ version: '9' }).setToken(process.env['token']);
 
-await rest.put(
-	Routes.applicationCommands(clientId),
-	{ body: commands },
-);
+		new SlashCommandBuilder().setName('botinfo').setDescription('Shows you bot infos! (some are important)'),
+		new SlashCommandBuilder().setName('debugify').setDescription('DEBUG COMMAND!, YOU SHOULDNT EXECUTE THIS!!'),
+	]
+		.map(command => command.toJSON())
+		
+	const rest = new REST({ version: '9' }).setToken(process.env['token']);
+	//const args = interaction.options.getString("filter");
+	(async () => {
+		try {
+			console.log('Started refreshing application (/) commands.');
+	
+			await rest.put(
+				Routes.applicationCommands(process.env['clientID']),
+				{ body: commands },
+			);
+	
+			console.log('Successfully reloaded application (/) commands.');
+		} catch (error) {
+			if (isDebugEnv)
+			{
+				console.error(error);
+			}
+			//
+		}
+	})();
+
+	module.exports = reloadCommands;
+	//module.exports = args;
+}
--- index.js
+++ index.js
@@ -3,61 +3,87 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const keepAlive = require('./server.js')
+const reloadCommands = require('./deploy-commands.js')
+const args = require('./deploy-commands.js')
+const os = require("os");
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+let osver = os.platform() + " " + os.release();
+let isDebugEnv = osver.includes("win32");
+
+const token = process.env['token']
+
 var fs = require("fs");
 
-
-
+// get OS info - get is debug env for debugging
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
-	
-	client.user.setPresence({
-        status: "online",  //You can show online, idle....
-        game: {
-            name: "24/7 Seedbank bot!, /botinfo to get started!",  //The message shown
-            type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }
-    });
+	console.log('[logger.Client] Client responded, OK!');
+
+	client.user.setPresence({ activities: [{ name: 'ready for use!, [insert cmd here] to get started!'}] });
 });
 
+
+
+
+// do context menu, so no diffrenet command, only 1 command any many context
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-
 	const { commandName } = interaction;
 
-	if (commandName === 'mapless') {
-		// kontol
-		var text = fs.readFileSync("./seedbank/mapless.txt", 'utf-8');
+	if (commandName === 'findseed') {
+		// NO WAY HOLY SHIT THE 5HEAD STUFF
+
+		/**var text = fs.readFileSync("./seedbank/mapless.txt", 'utf-8');
 		var seed = text.split("\n")
 		const random = Math.floor(Math.random() * seed.length);
-		console.log(random, seed[random]);
-		await interaction.reply("Seed: " + seed[random]);
-	} else if (commandName === "poweredvillage") { 
-		// kontol
-		var text = fs.readFileSync("./seedbank/wild.txt", 'utf-8');
-		var seed = text.split("\n")
-		const random = Math.floor(Math.random() * seed.length);
-		console.log(random, seed[random]);
-		await interaction.reply("Seed: " + seed[random]);
-	} else if (commandName === "coastalvillage") { 
-		// kontol
-		var text = fs.readFileSync("./seedbank/seedDatabaseaa.txt", 'utf-8');
-		var seed = text.split("\n")
-		const random = Math.floor(Math.random() * seed.length);
-		console.log(random, seed[random]);
-		await interaction.reply("Seed: " + seed[random]);
+		/*/
+
+		//YEAHHHH THIS WORKSSSS!!!!!!!
+		//IM SO HAPPY HOLY SHITT
+		if (interaction.options.getString("filter") == 'mapless')
+		{
+			console.log("[logger.Bot] yo you doin mapless?");
+			await interaction.reply("hi too");
+		}
+		else if (interaction.options.getString("filter") == 'yeah')
+		{
+			console.log("[logger.Bot] yo you not doin mapless?");
+			await interaction.reply("hi");
+		}
+
+		//await interaction.reply("should be after hi " + context + " " + commandName);
+		//console.log("[logger.Bot] " + random, seed[random]);
+		//await interaction.reply("Seed: " + seed[random]);
+		
 	} else if (commandName === 'botinfo') {
 		// kontol
-		console.log("user demmanded bot info!");
-		var interateText = fs.readFileSync("./commandOutputs/botinfo.txt", 'utf-8');
-		await interaction.reply(interateText);
+		console.log("[logger.Bot] " +"user demmanded bot info!");
+		await interaction.reply("```Seed Miner is a bot that can hand you seeds of your choosing, there are plenty of generators\n\nYou can check by using the [/] commands of Seed Miner\n\nSeed Miner uses a personal seedbank which uses a custom generator, if you want to contribute to seedfinding, please dm [Aeroshide#6200]\n\nThe bot is still in development, and still requires a bunch of generators, the target is to have every generator for everyone's needs\n\nChangelog [Update 13/07/2022] :\n[+] Mapless now has a nether filter (enter is still not guaranteed)\n\nGenerators that are planned to be released:\n1.14 Classic (in the works)\n1.15 Igloo (low priority)```");
+	} else if (commandName === 'debugify') {
+		// kontol
+		console.log("[logger.Bot] " +"user demmanded debug!");
+		await interaction.reply("Operating system : " + osver + "  is debug environment? : " + isDebugEnv);
 	}
+
 });
-	
+
 // Login to Discord with your client's token
 keepAlive();
+reloadCommands();
 client.login(process.env['token']);
+
+
+console.log("[logger.Client] client thread login created!, should be logging in to client!")
+
+if (isDebugEnv)
+{
+	console.log("[logger.Env] Client booted in debug environment!, some features may need to be altered to work on production mode!, bot token could also be leaked on source as well!!!")
+}
+else
+{
+	console.log("[logger.Env] Client booted in production environment!, you're good to go!")
+}
+
